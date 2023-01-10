ace6ee8fe7fbb48383d54e571f149173192d1705
sjojon <90796535+sjojon@users.noreply.github.com>
2023-01-10 14:07:08 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function farge() {
+    document.getElementById("bak").style.backgroundColor = "red";
+}
+
+function tilbake(){
+    document.getElementById("bak").style.backgroundColor = "lightgrey";
+}
